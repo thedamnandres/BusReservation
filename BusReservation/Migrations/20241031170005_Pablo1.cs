1fd9ccf5b569dcf5732e0f312c8b089cbb74e499
PabloTwoElf <pablo.escoba2005@gmail.com>
2024-10-31 12:03:14 -0500

    pruebarama

---
+++ BusReservation/Migrations/20241031170005_Pablo1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusReservation.Migrations
+{
+    /// <inheritdoc />
+    public partial class Pablo1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
